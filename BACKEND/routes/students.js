a44f54800c2340f26d8d35018b8ef7573e590d4f
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-04 13:57:24 +0530

    insert data : backend

---
+++ BACKEND/routes/students.js
@@ -0,0 +1,27 @@
+const router=require("express").Router();
+
+let Student =require("../models/Student");
+
+//https://localhost:8070/student/add
+router.route("/add").post((req,res)=>{
+    const name=req.body.name;
+    const age=Number(req.body.age);
+    const grat_year=Number(req.body.grat_year);
+
+    const newStudent= new Student({
+        name,
+        age,
+        grat_year
+    })
+
+    newStudent.save().then(()=>{
+        res.json("New Student Added !");
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
+
+
+module.exports=router;
