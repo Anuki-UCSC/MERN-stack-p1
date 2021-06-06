10091b6b542a1a8552163516a845d0e3ae26bd9a
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-06 15:18:24 +0530

    app frontend

--- frontendnew/src/App.js
+++ frontendnew/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import State_class from "./components/State_class";
+import  Counter_funtion from "./components/Counter_function";
+import Header from "./components/Header";
+import AddStudents from "./components/AddStudents";
+import {BrowserRouter as Router ,Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <Router>
+      <div>
+      <Header/>
+      <div className="container">
+          <State_class/>
+          < Counter_funtion/>
+          
+          <div className="container">
+          <Route path="./add" extract component={AddStudents}/>
+          </div>
+      
+      </div>
     </div>
+    </Router>
+    
   );
 }
 
