15fe05afef573c406d3ed246dcea9ece9a65bfc8
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-15 19:59:55 +0530

    get one student

---
+++ frontendnew/src/components/GetStudent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class GetStudent extends Component{
+    render(){
+        return(
+            <div>
+                Get stuents
+            </div>
+        )
+    }
+}
