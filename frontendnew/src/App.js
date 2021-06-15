18c4592697581634caa572e9fb60a6c5cb199d54
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-15 20:00:20 +0530

    import new components

--- frontendnew/src/App.js
+++ frontendnew/src/App.js
@@ -3,6 +3,10 @@ import State_class from "./components/State_class";
 import  Counter_funtion from "./components/Counter_function";
 import Header from "./components/Header";
 import AddStudents from "./components/AddStudents";
+import AllStudents from "./components/AllStudents";
+import EditStudent from "./components/EditStudent";
+import GetStudent from "./components/GetStudent";
+
 import {BrowserRouter as Router ,Route} from "react-router-dom";
 import axios from 'axios';
 import { Component } from 'react';
@@ -21,7 +25,9 @@ function App() {
           
           <div className="container">
           <Route path="./add" extract component={AddStudents}/>
-          <Route path="./add" extract component={AddStudents}/>
+          <Route path="./show" extract component={AllStudents}/>
+          <Route path="./edit/:id" extract component={EditStudent}/>
+          <Route path="student/:id" extract component={GetStudent}/>
           </div>
       
       </div>
