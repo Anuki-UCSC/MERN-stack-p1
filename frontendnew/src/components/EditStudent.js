7ff7024facc632583c4ac62275121290d4f2d01b
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-15 19:32:27 +0530

    created edit students class

---
+++ frontendnew/src/components/EditStudent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class EditStudents extends Component{
+    render(){
+        return(
+            <div>
+                edit stuents
+            </div>
+        )
+    }
+}
