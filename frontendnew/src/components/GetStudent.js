45ba334f4fb366865664e1acc4595f7eb9448e8a
Anuki-UCSC <alwis.anuki@gmail.com>
2021-06-16 07:37:51 +0530

    get a perticular student details

--- frontendnew/src/components/GetStudent.js
+++ frontendnew/src/components/GetStudent.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class GetStudent extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            student:[]
+        };
+    }
+
+    componentDidMount(){
+        const id=this.props.match.params.id;
+        axios.get(`http://localhost:8070/student/get/${id}`).then((res)=>{// there can be problem here
+            if(res.data.success){
+                this.setState({
+                    student:res.data.studentData // there can be problem here
+                });
+                console.log(this.state.student);
+            }
+            
+        });
+
+    }
+
     render(){
+
+        const {name , age, grat_year} = this.state.student;
+
         return(
-            <div>
-                Get stuents
-            </div>
+
+            <dl class="row">
+
+                <h1>Profile</h1>
+                <hr />
+                <dt class="col-sm-3"> </dt>
+                <dd class="col-sm-9"></dd>
+                <dt class="col-sm-3">Full Name </dt>
+                <dd class="col-sm-9">{name}</dd>
+
+                <dt class="col-sm-3">Age</dt>
+                <dd class="col-sm-9">{age}</dd>
+
+                <dt class="col-sm-3">Graduation Year</dt>
+                <dd class="col-sm-9">{grat_year}</dd>
+
+            </dl>
+            
         )
     }
 }
